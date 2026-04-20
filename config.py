b13f4bcc6ad060ec2f97e59a2d4d96ea0c69b1eb
Futo Sawaguchi <kaede.tyu3@icloud.com>
2026-04-20 09:46:08 +0900

    docs: update comments in config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ CAMERA_INDEX = 0
 YOLO_MODEL = "yolov8n.pt"
 YOLO_CONFIDENCE = 0.5
 
-# --- Azure ---
+# --- Azure Computer Vision ---
 AZURE_ENDPOINT = os.getenv("AZURE_CV_ENDPOINT")
 AZURE_API_KEY = os.getenv("AZURE_CV_API_KEY")
 AZURE_COOLDOWN_SEC = 3
